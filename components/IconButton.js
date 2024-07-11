82b09459b86b56f3113109c299330d4390c65155
Михаел Йорданов <gaws@mail.bg>
2024-07-11 14:10:22 +0300

    Add Icon Button

---
+++ components/IconButton.js
@@ -0,0 +1,23 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function IconButton({ icon, onPress, label }) {
+  return (
+    <Pressable style={styles.iconButton} onPress={onPress}>
+      <MaterialIcons name={icon} size={24} color="#fff" />
+      <Text style={styles.iconButtonLabel}>{label}</Text>
+    </Pressable>
+  );
+}
+
+export default IconButton;
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconButtonLabel: {
+    color: "#fff",
+    marginTop: 12,
+  },
+});
